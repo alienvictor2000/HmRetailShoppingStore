149ca3986982d6f6c3cda201f7b4e9d43c766569
alienvictor2000 <61653833+alienvictor2000@users.noreply.github.com>
2021-03-21 22:19:48 +0700

    Update controller.js

--- src/main/webapp/WEB-INF/resources/js/controller.js
+++ src/main/webapp/WEB-INF/resources/js/controller.js
@@ -3,13 +3,13 @@ var cartApp = angular.module ("cartApp", []);
 cartApp.controller("cartCtrl", function ($scope, $http){
 
     $scope.refreshCart = function () {
-        $http.get('huy-shopping-store.herokuapp.com/rest/cart/'+$scope.cartId).success(function (data) {
+        $http.get('/rest/cart/'+$scope.cartId).success(function (data) {
             $scope.cart=data;
         });
     };
 
     $scope.clearCart = function () {
-        $http.delete('huy-shopping-store.herokuapp.com/rest/cart/'+$scope.cartId).success($scope.refreshCart());
+        $http.delete('/rest/cart/'+$scope.cartId).success($scope.refreshCart());
     };
 
     $scope.initCartId = function (cartId) {
@@ -18,13 +18,13 @@ cartApp.controller("cartCtrl", function ($scope, $http){
     };
 
     $scope.addToCart = function (productId) {
-        $http.put('huy-shopping-store.herokuapp.com/rest/cart/add/'+productId).success(function () {
+        $http.put('/rest/cart/add/'+productId).success(function () {
             alert("Product successfully added to the cart!")
         });
     };
 
     $scope.removeFromCart = function (productId) {
-        $http.put('huy-shopping-store.herokuapp.com/rest/cart/remove/'+productId).success(function (data) {
+        $http.put('/rest/cart/remove/'+productId).success(function (data) {
             $scope.refreshCart();
         });
     };
